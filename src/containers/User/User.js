415501b8bf93ed5ea7e82593825a807b89500028
DESKTOP-MFQCCVU\yazid <yazid@decision-science.com>
2019-04-05 18:24:50 +0800

    Add activeUserId reducer

--- src/containers/User/User.js
+++ src/containers/User/User.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './User.css';
+import store from '../../store';
+import { setActiveUserId } from '../../actions/index';
 
 const User = ({ user }) => {
     const { name, profile_pic, status } = user;
 
     return (
-        <div className='User'>
+        <div className='User' onClick={handleUserClick.bind(null, user)}>
             <img src={profile_pic} alt={name} className="User__pic" />
             <div className='User__details'>
                 <p className='User__details-name'>{name}</p>
@@ -14,5 +16,9 @@ const User = ({ user }) => {
         </div>
     );
 };
+
+function handleUserClick({ user_id }) {
+    store.dispatch(setActiveUserId(user_id));
+}
 
 export default User;
