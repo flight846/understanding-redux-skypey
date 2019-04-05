f19e03969f0ea1ad5914af82d8725d10022e78d3
DESKTOP-MFQCCVU\yazid <yazid@decision-science.com>
2019-04-05 16:08:13 +0800

    Add empty and chatWindow component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,17 @@
-import React from "react";
-import "./Main.css";
+import React from 'react';
+import './Main.css';
+import Empty from '../Empty/Empty';
+import ChatWindow from '../../containers/ChatWindow/ChatWindow';
 
-const Main = () => {
-    return <main className="Main">Main Stuff</main>;
+const Main = ({ user, activeUserId }) => {
+    const renderMainContent = 
+        !activeUserId ? (
+            <Empty user={user} activeUserId={activeUserId} />
+        ) : (
+            <ChatWindow activeUserId={activeUserId} />
+        )
+
+    return <main className='Main'>{ renderMainContent }</main>;
 };
 
 export default Main;
