c317bcd4653e03553df8a0b4375d670bf3e9cc62
yazid <yazid@decision-science.com>
2019-04-07 20:28:34 +0800

    Add chats

--- src/containers/ChatWindow/ChatWindow.js
+++ src/containers/ChatWindow/ChatWindow.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React from 'react';
+import _ from 'lodash';
+
 import store from '../../store';
 import Header from '../../components/Header/Header';
 import Chats from '../../components/Chats/Chats';
-import './ChatWindow.css';
-import _ from 'lodash';
+import MessageInput from '../../components/MessageInput/MessageInput';
+
+import './ChatWindow.css'
 
 const ChatWindow = ({ activeUserId }) => {
-    const state = store.getState();
-    const activeUser = state.contacts[activeUserId];
-    const activeMsgs = state.messages[activeUserId];
+    const state = store.getState()
+    const activeUser = state.contacts[activeUserId]
+    const activeMsgs = state.messages[activeUserId]
+    const { typing } = state
 
     return (
         <div className="ChatWindow">
             <Header user={activeUser} />
             <Chats messages={_.values(activeMsgs)} />
+            <MessageInput value={typing} />
         </div>
-    );
-};
+    )
+}
 
 export default ChatWindow;
